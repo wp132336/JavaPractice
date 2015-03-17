6598e194447b3cb866cd6c4b0845dfe493eaded7
하상혁 <@hellbenter>
2015-03-17 11:33:34 +0900

    첫번째 실습과제 제출

---
+++ JavaPractice/src/org/dimigo/basic/Propeal.java
@@ -0,0 +1,42 @@
+package org.dimigo.basic;
+
+public class Propeal {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("<<아이유 프로필>>");
+		
+		String name = "아이유";
+			
+		boolean s= true;
+		
+		System.out.println("이름 : " + name);
+		if(s) {
+			System.out.println("성별 : " + "여자");
+			}
+		else {
+			System.out.println("성별 : " + "남자");
+		}
+		
+		int age = 23;
+		
+		System.out.println("나이 : " + age);
+		
+		double high = 161.8;
+		
+		System.out.println("키 : " + high + "cm");
+		
+		float weight = 161.8f;
+		
+		System.out.println("몸무게 : "+ weight + "kg");
+		
+		char bloodtype = 'a';
+		
+		System.out.println("혈액형 : "+ bloodtype + "형");
+		//System.out.println();
+		//System.out.println(f2);
+		//System.out.println(f2);
+		//System.out.println(f2);
+	}
+
+}
